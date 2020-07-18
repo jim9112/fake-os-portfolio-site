7c18d43930d278778623484bd00ba5cebc7b8faa
jim9112 <jim9112@gmail.com>
2020-07-18 18:55:56 -0400

    Drag and drop about me

--- app.js
+++ app.js
@@ -1,1 +1,43 @@
-console.log('test');
+const aboutMeWindow = document.querySelector('.aboutMe');
+const aboutMeHeader = document.querySelector('.windowHeader');
+
+const handleMouseDown = (element) => {
+  let windowPosX = 0;
+  let windowPosY = 0;
+  let mousePosX = 0;
+  let mousePosY = 0;
+
+  const dragMouseDown = (e) => {
+    e = e || window.event;
+    e.preventDefault();
+    // get mouse cursor position at start
+    mousePosX = e.clientX;
+    mousePosY = e.clientY;
+    // close function on mouse up
+    document.onmouseup = closeDragElement;
+    // listen for mouse move
+    document.onmousemove = elementDrag;
+  };
+  const elementDrag = (e) => {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate new cursor position
+    windowPosX = mousePosX - e.clientX;
+    windowPosY = mousePosY - e.clientY;
+    mousePosX = e.clientX;
+    mousePosY = e.clientY;
+    // set elements new position
+    element.style.top = element.offsetTop - windowPosY + 'px';
+    element.style.left = element.offsetLeft - windowPosX + 'px';
+  };
+  function closeDragElement() {
+    // stop moving when mouse button is released:
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+  dragMouseDown();
+};
+
+aboutMeHeader.addEventListener('mousedown', () =>
+  handleMouseDown(aboutMeWindow)
+);
