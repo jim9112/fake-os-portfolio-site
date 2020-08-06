9951c69d5525d8279741b7e049a6686a9f4d7096
jim9112 <jim9112@gmail.com>
2020-08-06 15:44:43 -0400

    Made the drag function universal

--- app.js
+++ app.js
@@ -1,71 +1,72 @@
 const aboutMeWindow = document.querySelector('.aboutMe');
-const aboutMeHeader = document.querySelector('.windowHeader');
-const aboutMeExitButton = document.querySelector('.aboutExit');
 const aboutMeIcon = document.querySelector('.aboutMeIcon');
 
-// handle drag and drop for windows
-const handleMouseDown = (element) => {
-  let windowPosX = 0;
-  let windowPosY = 0;
-  let mousePosX = 0;
-  let mousePosY = 0;
+// handles the opening and closing and movement of windows
+function HandleWindow(selectedWindow, icon) {
+  const exitButton = selectedWindow.querySelector('.exit');
+  const windowHeader = selectedWindow.querySelector('.windowHeader');
 
-  const dragMouseDown = (e) => {
-    e = e || window.event;
-    e.preventDefault();
-    // get mouse cursor position at start
-    mousePosX = e.clientX;
-    mousePosY = e.clientY;
-    // close function on mouse up
-    document.onmouseup = closeDragElement;
-    // listen for mouse move
-    document.onmousemove = elementDrag;
+  const handleMouseDown = (element) => {
+    let windowPosX = 0;
+    let windowPosY = 0;
+    let mousePosX = 0;
+    let mousePosY = 0;
+
+    const dragMouseDown = (e) => {
+      e = e || window.event;
+      e.preventDefault();
+      // get mouse cursor position at start
+      mousePosX = e.clientX;
+      mousePosY = e.clientY;
+      // close function on mouse up
+      document.onmouseup = closeDragElement;
+      // listen for mouse move
+      document.onmousemove = elementDrag;
+    };
+    const elementDrag = (e) => {
+      e = e || window.event;
+      e.preventDefault();
+      // calculate new cursor position
+      windowPosX = mousePosX - e.clientX;
+      windowPosY = mousePosY - e.clientY;
+      mousePosX = e.clientX;
+      mousePosY = e.clientY;
+      // set elements new position
+      element.style.top = element.offsetTop - windowPosY + 'px';
+      element.style.left = element.offsetLeft - windowPosX + 'px';
+    };
+    function closeDragElement() {
+      // stop moving when mouse button is released:
+      document.onmouseup = null;
+      document.onmousemove = null;
+    }
+    dragMouseDown();
   };
-  const elementDrag = (e) => {
-    e = e || window.event;
-    e.preventDefault();
-    // calculate new cursor position
-    windowPosX = mousePosX - e.clientX;
-    windowPosY = mousePosY - e.clientY;
-    mousePosX = e.clientX;
-    mousePosY = e.clientY;
-    // set elements new position
-    element.style.top = element.offsetTop - windowPosY + 'px';
-    element.style.left = element.offsetLeft - windowPosX + 'px';
+
+  // handle close window
+  const closeWindow = (element, icon) => {
+    element.style.display = 'none';
+    icon.classList.remove('fa-folder-open');
+    icon.classList.add('fa-folder');
   };
-  function closeDragElement() {
-    // stop moving when mouse button is released:
-    document.onmouseup = null;
-    document.onmousemove = null;
-  }
-  dragMouseDown();
-};
 
-// handle close window
-const closeWindow = (element, icon) => {
-  element.style.display = 'none';
-  icon.classList.remove('fa-folder-open');
-  icon.classList.add('fa-folder');
-};
+  // handle open window
+  const openWindow = (element, icon) => {
+    element.style.display = '';
+    icon.classList.remove('fa-folder');
+    icon.classList.add('fa-folder-open');
+  };
+  // ************* Event Listeners ***************
+  // drag event listener
+  windowHeader.addEventListener('mousedown', () =>
+    handleMouseDown(selectedWindow)
+  );
 
-// handle open window
-const openWindow = (element, icon) => {
-  element.style.display = '';
-  icon.classList.remove('fa-folder');
-  icon.classList.add('fa-folder-open');
-};
-// ************* Event Listeners ***************
-// about me drag event listener
-aboutMeHeader.addEventListener('mousedown', () =>
-  handleMouseDown(aboutMeWindow)
-);
+  // exit button event listener
+  exitButton.addEventListener('click', () => closeWindow(selectedWindow, icon));
 
-// about me exit button event listener
-aboutMeExitButton.addEventListener('click', () =>
-  closeWindow(aboutMeWindow, aboutMeIcon)
-);
+  // icon listener
+  icon.addEventListener('click', () => openWindow(selectedWindow, icon));
+}
 
-// about me icon listener
-aboutMeIcon.addEventListener('click', () =>
-  openWindow(aboutMeWindow, aboutMeIcon)
-);
+const aboutWindow = HandleWindow(aboutMeWindow, aboutMeIcon);
