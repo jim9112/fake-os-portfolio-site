7a994b733d43099db1dc18543082d17447a1d1b4
jim9112 <jim9112@gmail.com>
2020-11-04 18:26:27 -0500

    removed unused html and js

--- app.js
+++ app.js
@@ -7,11 +7,8 @@ import myProfile from './js/myProfile.js';
 import { createNewWindow, HandleWindow } from './js/utils.js';
 import { Content } from './js/lib.js';
 
-const aboutMeWindow = document.querySelector('.aboutMe');
-const projectsWindow = document.querySelector('.projects');
 const aboutMeIcon = document.querySelector('.aboutMeIcon');
 const projectsIcon = document.querySelector('.projectsIcon');
-const projectIconWindow = document.querySelector('.projectIconWindow');
 
 // generate icons for each of the portfolio projects
 const createProjectsIcons = () => {
@@ -28,23 +25,24 @@ const createProjectsIcons = () => {
   });
   return iconHTML.join(' ');
 };
+
+// generate new project window
 const projectWindowContent = new Content(
   createProjectsIcons(),
   'projectIconWindow'
 );
 
+// generate about me window
 const aboutMeWindowContent = new Content(myProfile.content, 'indent');
-// on page load functions
-// createProjectsIcons();
-// projectIconWindow.innerHTML = createProjectsIcons();
 
-const aboutWindow = HandleWindow(aboutMeWindow, aboutMeIcon);
-// const myProjectsWindow = HandleWindow(projectsWindow, projectsIcon);
 // event handlers
+
+// handler for about me window
 aboutMeIcon.addEventListener('click', (e) => {
   const { content, classlist } = aboutMeWindowContent;
   createNewWindow(e, content, classlist);
 });
+
 // project window event handler
 projectsIcon.addEventListener('click', (e) => {
   const { content, classlist } = projectWindowContent;
